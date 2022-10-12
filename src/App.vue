715c4589cb2cbf7424cab6fb4dc3e8a178418c90
Jordan Eggers <jordaneggers@Jordans-MBP.lan>
2022-10-12 10:51:37 -0400

    right before learning slots

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <Modal />
+    <p>Welcome..</p>
+    <div v-if="showModal">
+      <Modal :header="header" :text="text" theme="sale" @close="toggleModal"/>
+    </div>
+    <button @click="toggleModal">open modal</button>
     <input type="text" ref="name" />
     <button @click="handleClick">click me</button>
   </div>
@@ -15,7 +19,10 @@ export default {
   components: { Modal },
   data() {
     return {
-      title: 'My First Vue App :)'
+      title: 'My First Vue App :)',
+      header: 'Sign up for the Giveaway!',
+      text: 'Grab your ninja swag for half price!',
+      showModal: false
     }
   },
   methods: {
@@ -23,6 +30,9 @@ export default {
       console.log(this.$refs.name)
       this.$refs.name.classList.add('active')
       this.$refs.name.focus()
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
     }
   }
 }
